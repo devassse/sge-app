76e7d75417fcf1d29bacf2e65008aaae53249170
Joao Devson Mucavel <joao.mucavel@ideiasdinamicas.com>
2023-07-12 10:02:02 +0200

    Users

--- src/pages/CompaniesPage.vue
+++ src/pages/CompaniesPage.vue
@@ -1,7 +1,91 @@
 <template>
-  <q-page class="flex flex-center">
-    <h2>Empresas</h2>
+  <q-page class="">
+    <div class="add-company">
+      <q-btn round color="black" icon="add" class="add-btn" />
+    </div>
+    <div class="row">
+      <div class="col-3 q-pa-sm" v-for="company of companies" :key="company.id">
+        <q-card class="my-card" flat bordered>
+          <q-img src="../assets/cover/cover.jpeg" style="height: 140px" />
+
+          <q-card-section>
+            <q-btn
+              fab
+              color="primary"
+              icon="place"
+              class="absolute"
+              style="top: 0; right: 12px; transform: translateY(-50%)"
+            />
+
+            <div class="row no-wrap items-center">
+              <div class="col text-h6 ellipsis">{{ company.name }}</div>
+              <div
+                class="col-auto text-grey text-caption q-pt-md row no-wrap items-center"
+              >
+                <q-icon name="place" />
+                250 ft
+              </div>
+            </div>
+
+            <q-rating v-model="stars" :max="5" size="32px" />
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            <div class="text-subtitle1">{{ company.type }}</div>
+            <div class="text-caption text-grey">
+              {{ company.description }}
+            </div>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
   </q-page>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+let companies = ref([
+  {
+    id: 1,
+    name: "Empresa de Gas",
+    type: "Extraction and Encapsulation",
+    description: "Lorem ipsum dolor sit amet redat, dolor sit amet redat.",
+  },
+  {
+    id: 2,
+    name: "ENH",
+    type: "Managment and Investigation",
+    description: "Lorem ipsum dolor sit amet redat, rem ipsum dolor sit ame.",
+  },
+  {
+    id: 3,
+    name: "Banco de Mozambique",
+    type: "Financial Institution",
+    description: "Lorem ipsum dolor sit amet redat dolor sit amet redat",
+  },
+  {
+    id: 4,
+    name: "Transportes Lalgy",
+    type: "Transportation and Logistic",
+    description: "Lorem ipsum dolor sit amet redat rem ipsum dolor sit ame",
+  },
+  {
+    id: 5,
+    name: "Feira Popular",
+    type: "Popular and Entertaiment",
+    description: "Lorem ipsum dolor sit amet redat rem ipsum dolor sit ame",
+  },
+]);
+</script>
+<style scoped lang="scss">
+.add-btn {
+  position: relative;
+  left: 81vw;
+  right: 0;
+  top: 4px;
+}
+.add-btn:hover {
+  transform: rotate(360deg);
+}
+</style>
--- src/pages/EmployeesPage.vue
+++ src/pages/EmployeesPage.vue
@@ -1,7 +1,90 @@
 <template>
-  <q-page class="flex flex-center">
-    <h2>Funcionários</h2>
+  <q-page class="">
+    <div class="add-employee">
+      <q-btn round color="black" icon="add" class="add-btn" />
+    </div>
+    <div class="row">
+      <div
+        class="col-3 q-pa-sm"
+        v-for="employee of employees"
+        :key="employee.id"
+      >
+        <q-card class="my-card" flat bordered>
+          <q-img src="../assets/cover/user-cover.jpg" style="height: 130px" />
+
+          <q-card-section>
+            <q-img
+              class="absolute"
+              src="../assets/cover/user.png"
+              style="width: 50px; right: 10px; top: -25px"
+            />
+
+            <div class="row no-wrap items-center">
+              <div class="col text-h6 ellipsis">{{ employee.name }}</div>
+              <div
+                class="col-auto text-grey text-caption q-pt-md row no-wrap items-center"
+              >
+                <q-icon name="star" />
+                250 Pts
+              </div>
+            </div>
+
+            <q-rating v-model="stars" :max="5" size="32px" />
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            <div class="text-subtitle1">{{ employee.type }}</div>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
   </q-page>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+let employees = ref([
+  {
+    id: 1,
+    name: "Manuel José de Almeida",
+    type: "Administrador",
+    description: "Lorem ipsum dolor sit amet redat, dolor sit amet redat.",
+  },
+  {
+    id: 2,
+    name: "Maria Jorge",
+    type: "Managment and Investigation",
+    description: "Lorem ipsum dolor sit amet redat, rem ipsum dolor sit ame.",
+  },
+  {
+    id: 3,
+    name: "Bento Nhantumbo",
+    type: "Financial Institution",
+    description: "Lorem ipsum dolor sit amet redat dolor sit amet redat",
+  },
+  {
+    id: 4,
+    name: "Mario Sergio",
+    type: "Transportation and Logistic",
+    description: "Lorem ipsum dolor sit amet redat rem ipsum dolor sit ame",
+  },
+  {
+    id: 5,
+    name: "Freya Abdul Alibah",
+    type: "Popular and Entertaiment",
+    description: "Lorem ipsum dolor sit amet redat rem ipsum dolor sit ame",
+  },
+]);
+</script>
+<style scoped lang="scss">
+.add-btn {
+  position: relative;
+  left: 81vw;
+  right: 0;
+  top: 4px;
+}
+.add-btn:hover {
+  transform: rotate(360deg);
+}
+</style>

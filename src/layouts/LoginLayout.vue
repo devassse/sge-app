b5ce7d910fea2c30b8f8eee8cb8a37f7c7de96af
Joao Devson Mucavel <joao.mucavel@ideiasdinamicas.com>
2023-07-12 08:53:36 +0200

    login page

--- src/layouts/LoginLayout.vue
+++ src/layouts/LoginLayout.vue
@@ -1,17 +1,51 @@
 <template>
-  <q-layout
-    view=""
-    style="
-      background-color: #5690fc;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    "
-  >
+  <q-layout class="login-page" view="">
     <div>
-      <span class="text-h2">Login</span>
+      <div class="logo">
+        <img src="../assets/logo/logo.png" alt="" />
+      </div>
+
+      <div class="login-form">
+        <q-input dense color="blue" v-model="username" label="Utilizador">
+          <template v-slot:prepend>
+            <q-icon name="group" />
+          </template>
+        </q-input>
+        <q-separator size="2.2rem" color="blue-1" />
+        <q-input
+          dense
+          color="blue"
+          v-model="username"
+          label="Password"
+          type="password"
+        >
+          <template v-slot:prepend>
+            <q-icon name="group" />
+          </template>
+        </q-input>
+
+        <q-separator size="2.2rem" color="blue-1" />
+        <q-btn color="primary" label="Log In" style="width: 100%" />
+      </div>
     </div>
   </q-layout>
 </template>
 <script setup></script>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.login-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.logo {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 2.2rem;
+}
+.login-form {
+  width: 460px;
+  padding: 2.2rem;
+  background-color: #e3f2fd;
+  border-radius: 10px;
+}
+</style>
